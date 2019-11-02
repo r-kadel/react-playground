d39285c77f452938f46b858d56f490373472d6f8
Ryan Kadel <ryan.kadel88@gmail.com>
2019-11-02 08:49:08 -0400

    wtf commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
 
+const firstTooltip = (
+  <Tooltip message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
-      <Split />
-      <Split />
-      <Split />
-      <Split />
+      <Split className='left' flexBasis='2'>
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
